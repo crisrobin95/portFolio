507e982736e9f743b82f0e99102c75f5882a4fc9
CristhianQuiñonez <crisrobin-95@hotmail.com>
2026-03-24 20:25:37 +0100

    Refactor(default.vue): mejora la estructura del layout y añade estilos SCSS

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,32 @@
-<script setup></script>
+<script setup lang="ts"></script>
 <template>
-  <Header></Header>
-  <main class="main">
-    <slot />
-  </main>
+  <div class="layout-container">
+    <header class="main-header">
+      <Header />
+    </header>
+
+    <main id="main-content">
+      <NuxtPage />
+    </main>
+
+    <footer class="main-footer">
+      <p>© 2026 Mi Portfolio</p>
+    </footer>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.layout-container {
+  @include main.flex(column, $gap: 1rem);
+}
+#main-content {
+  @include main.flex(column);
+  width: 100%;
+  height: 80dvh;
+}
+.main-header {
+  width: 100%;
+  min-height: 0.5dvh;
+  max-width: 120rem;
+}
+</style>
