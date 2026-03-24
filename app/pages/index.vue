d99ae68664510f267bc89f0670085268cd4c6677
CristhianQuiñonez <crisrobin-95@hotmail.com>
2026-03-24 18:45:55 +0100

    Refactor(index.vue): incorporar metadatos en head y ajusta estilos

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,18 @@
-<script setup>
+<script setup lang="ts">
+const { cv } = useCvData()
 useHead({
   title: 'Inicio',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Bienvenid@ a mi porfolio profesional. Conoce mis hhabilidades y proyectos',
+    },
+    {
+      property: 'og:title',
+      content: 'Porfolio de ' + (cv.value?.basics?.name || 'Usuario'),
+    },
+  ],
 })
 </script>
 <template>
@@ -15,20 +27,20 @@ useHead({
 </template>
 
 <style lang="scss" scoped>
-.contenedor-index{
+.contenedor-index {
+  @include main.flex(column, $gap: 1rem);
+  position: relative;
   max-width: 120rem;
-  
+  width: 100%;
 }
 .actions {
-  position: fixed;
-  right: 1rem;
-  @include main.flex();
+  position: absolute;
+  right: 1.5rem;
   z-index: 999;
   @include main.responsive(50rem) {
     position: initial;
     @include main.flex(column, $wrap: wrap, $gap: 1rem);
   }
 }
-
 @include main.responsive();
 </style>
