0e9e5bf63c2650e927a61de368b596e338e32004
CristhianQuiñonez <crisrobin-95@hotmail.com>
2026-03-30 23:48:52 +0200

    Refactor(habilidades.vue): mejora la estructura y presentación de la sección de habilidades

--- app/pages/habilidades.vue
+++ app/pages/habilidades.vue
@@ -1,58 +1,99 @@
 <script setup lang="ts">
 useHead({
   title: 'Habilidades',
+  meta: [
+    {
+      name: 'description',
+      content: 'Explora mi stack tecnológico: Vue, Nuxt, SCSS y más.',
+    },
+  ],
 })
+const titlePage = 'Habilidades Técnicas y Stack Tecnológico de Cristhian Quiñonez'
 </script>
 <template>
-  <main class="contenedor-habil">
-    <img src="../assets/fondo-habilidades.webp" alt="" />
+  <main class="container-skills">
+    <h1 class="sr-only">{{ titlePage }}</h1>
+    <NuxtImg
+      src="/fondo-habilidades.webp"
+      alt="Fondo de sección habilidades"
+      class="background-img"
+      loading="lazy"
+    />
     <aside class="button-prev">
-      <NuxtLink to="/experiencia">
-        <BottonNext label="Experiencia" />
+      <NuxtLink to="/">
+        <BottonNext label="Sobre mí" />
       </NuxtLink>
     </aside>
     <section class="card">
       <CardSkils />
     </section>
     <aside class="button-next">
-      <NuxtLink to="/">
-        <BottonNext label="Sobre Mí" />
+      <NuxtLink to="/experiencia">
+        <BottonNext label="Experienca" />
       </NuxtLink>
     </aside>
   </main>
 </template>
 
 <style scoped lang="scss">
-.contenedor-habil{
-  max-width: 120rem;
-}
-img {
-  position: absolute;
-  z-index: -1;
+.container-skills {
+  position: relative;
+  @include main.flex(column, $gap: 2rem);
   width: 100%;
+  box-sizing: border-box;
   max-width: 120rem;
-  height: 80%;
-  max-height: 67.5rem;
+  z-index: 1;
+
+  .background-img {
+    padding-top: 1rem;
+    position: absolute;
+    width: 99%;
+    height: 85dvh;
+    z-index: -1;
+    opacity: 0.8;
+  }
 }
 
 .card {
   @include main.flex();
 }
 
-.button-prev {
-  position: fixed;
-  margin: 1rem;
-  right: 1rem;
-  bottom: 1rem;
-  @include main.flex();
-  z-index: 999;
-}
-
 .button-next {
   position: fixed;
-  margin: 1rem;
-  bottom: 1rem;
-  @include main.flex();
+  bottom: 5rem;
   z-index: 999;
+  @include main.flex();
+  left: 50%;
+  transform: translateX(calc(60rem - 100% - 1rem));
+
+  @include main.responsive(120rem) {
+    left: auto;
+    right: 1rem;
+    transform: none;
+  }
+
+  @include main.responsive(50rem) {
+    position: initial;
+    margin: 1rem auto;
+  }
+}
+
+.button-prev {
+  position: fixed;
+  bottom: 5rem;
+  z-index: 999;
+  @include main.flex();
+  left: 50%;
+  transform: translateX(calc(-60rem + 1rem));
+
+  @media (max-width: 120rem) {
+    left: 1rem;
+    transform: none;
+  }
+
+  @include main.responsive(50rem) {
+    position: initial;
+    margin: 1rem auto;
+  }
 }
 </style>
