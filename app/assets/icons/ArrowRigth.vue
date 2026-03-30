7cd54c1ea0cec926fee8e58889841ab5fceb9975
CristhianQuiñonez <crisrobin-95@hotmail.com>
2026-03-30 23:48:10 +0200

    Añadir componente ArrowRigth.vue con icono SVG para navegación

---
+++ app/assets/icons/ArrowRigth.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="1.5"
+    class="size-6"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+    />
+  </svg>
+</template>
