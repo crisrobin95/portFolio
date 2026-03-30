2778099221e2048fc5ba44914ad95647101bf3a1
CristhianQuiñonez <crisrobin-95@hotmail.com>
2026-03-30 23:48:30 +0200

    Refactor(CardSkils.vue): mejora la navegación y la presentación de habilidades

--- app/components/CardSkils.vue
+++ app/components/CardSkils.vue
@@ -1,19 +1,25 @@
 <script setup lang="ts">
+import ArrowLeft from '~/assets/icons/ArrowLeft.vue'
+import ArrowRigth from '~/assets/icons/ArrowRigth.vue'
 const { cv } = useCvData()
 const skillsList = computed(() => cv.value?.skills || [])
 
-// índice de la carta seleccionada
 const activeIndex = ref(0)
 
 function next() {
-  activeIndex.value = (activeIndex.value + 1) % skillsList.value.length
+  const total = skillsList.value.length
+  if (total > 0) {
+    activeIndex.value = (activeIndex.value + 1) % total
+  }
 }
 
 function prev() {
-  activeIndex.value =
-    (activeIndex.value - 1 + skillsList.value.length) % skillsList.value.length
+  const total = skillsList.value.length
+  if (total > 0) {
+    activeIndex.value = (activeIndex.value - 1 + total) % total
+  }
 }
-const radius = 300 // distancia desde el centro
+const radius = 450 // distancia desde el centro
 
 function getStyle(i: number | string) {
   const idx = Number(i)
@@ -32,12 +38,25 @@ function getStyle(i: number | string) {
     zIndex: isActive ? 1 : 0.5,
   }
 }
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyDown)
+})
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleKeyDown)
+})
+function handleKeyDown(event: KeyboardEvent) {
+  if (event.key === 'ArrowRight') {
+    next()
+  } else if (event.key === 'ArrowLeft') {
+    prev()
+  }
+}
 </script>
 
 <template>
   <div class="roulette-container">
-    <div class="roulette">
-      <div
+    <ul class="roulette">
+      <li
         v-for="(skill, i) in skillsList"
         :key="skill.name"
         class="skill-card"
@@ -45,42 +64,18 @@ function getStyle(i: number | string) {
       >
         <div class="head">
           <h3>{{ skill.name }}</h3>
-          <NuxtImg :src="skill.img" width="60" />
+          <NuxtImg :src="skill.img" width="65" />
         </div>
-        <p class="parrafo">{{ skill.description }}</p>
-      </div>
-    </div>
+        <p class="parrafo">{{ skill?.description }}</p>
+      </li>
+    </ul>
 
     <div class="controls">
       <button @click="prev">
-        <svg viewBox="0 0 24 24" fill="#fff" class="size-6">
-          <path
-            fill-rule="evenodd"
-            d="M10.72 11.47a.75.75 0 0 0 0 1.06l7.5 7.5a.75.75 0 1 0 1.06-1.06L12.31 12l6.97-6.97a.75.75 0 0 0-1.06-1.06l-7.5 7.5Z"
-            clip-rule="evenodd"
-          />
-          <path
-            fill-rule="evenodd"
-            d="M4.72 11.47a.75.75 0 0 0 0 1.06l7.5 7.5a.75.75 0 1 0 1.06-1.06L6.31 12l6.97-6.97a.75.75 0 0 0-1.06-1.06l-7.5 7.5Z"
-            clip-rule="evenodd"
-          />
-        </svg>
+        <ArrowLeft />
       </button>
       <button @click="next">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="size-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
-          />
-        </svg>
+        <ArrowRigth />
       </button>
     </div>
   </div>
@@ -89,11 +84,16 @@ function getStyle(i: number | string) {
 <style scoped lang="scss">
 .roulette-container {
   position: relative;
-  margin-top: 15rem;
-  perspective: 62.5rem;
+  margin-top: 10rem;
+  perspective: 100.5rem;
   width: 100%;
   max-width: 120rem;
   @include main.flex(column);
+
+  @include main.responsive(50rem) {
+    perspective: 1000rem;
+    @include main.flex();
+  }
 }
 .head {
   @include main.flex($gap: 1rem);
@@ -106,6 +106,10 @@ function getStyle(i: number | string) {
   position: relative;
   transform-style: preserve-3d;
   transition: transform 0.8s cubic-bezier(0.17, 0.67, 0.47, 1.39);
+  @include main.responsive(50rem) {
+    width: 5rem;
+    height: 6rem;
+  }
 }
 h3 {
   font-size: 0.9rem;
@@ -124,7 +128,7 @@ h3 {
   max-width: none;
   min-height: 10rem;
   border-radius: 1rem;
-  border: 1px solid white;
+  border: 0.0625rem solid white;
   transition:
     transform 0.8s ease,
     opacity 0.4s ease;
@@ -132,15 +136,18 @@ h3 {
 
 .controls {
   @include main.flex($gap: 15rem, $justify: space-evenly);
+  @include main.responsive(50rem) {
+    @include main.flex($wrap: wrap);
+  }
 }
 
 button {
-  width: 5rem;
+  width: 4rem;
   padding: 0.5rem 1rem;
   border-radius: 0.5rem;
   background: #242424;
   color: white;
-  border: 1px solid #444;
+  border: 0.0625rem solid #444;
   font-size: 1rem;
   font-weight: bold;
   cursor: pointer;
