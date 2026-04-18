20149a1c86f3396b481784d977c3462e019c966f
CristhianQuiñonez <crisrobin-95@hotmail.com>
2026-04-18 12:37:47 +0200

    Agregar estructura y estilo a la página de experiencia laboral

--- app/pages/experiencia.vue
+++ app/pages/experiencia.vue
@@ -1,7 +1,63 @@
+<script setup lang="ts">
+useHead({
+  title: 'Experiencia',
+  meta: [
+    {
+      name: 'description',
+      content: 'Aquí encontraras mí experiencia laboral',
+    },
+  ],
+})
+</script>
+
 <template>
-  <div>Esta es la página de experiencia</div>
+  <main class="container-experiencia">
+    <NuxtImg
+      src="/fondo-experiencia.webp"
+      alt="Fondo sección experiencia"
+      class="background-img"
+      loading="lazy"
+    />
+
+    <section class="content">
+      <CardExperience />
+    </section>
+
+    <aside class="actions">
+      <NuxtLink to="/">
+        <BottonNext label="Proyectos" class="botton-next" />
+      </NuxtLink>
+    </aside>
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<style lang="scss" scoped>
+.container-experiencia {
+  position: relative;
+  justify-content: center;
+  width: 99%;
+  min-height: 100dvh;
+  @include main.flex();
+  padding: 2rem 1rem;
+  overflow-x: hidden;
+}
 
-<style scoped></style>
+.background-img {
+  position: absolute;
+  width: 100%;
+  height: 100dvh;
+  object-fit: cover;
+  z-index: 0;
+  opacity: 0.3;
+}
+
+.content {
+  width: 100%;
+  flex: 1;
+}
+
+.actions {
+  margin-top: 1rem;
+  z-index: 10;
+}
+</style>
