b103135b8eb6d573eebd272164b8728028dc5923
CristhianQuiñonez <crisrobin-95@hotmail.com>
2026-04-28 11:12:25 +0200

    Agregar interfaz UiTranslations para la gestión de traducciones de la interfaz

---
+++ app/types/ui.ts
@@ -0,0 +1,20 @@
+export interface UiTranslations {
+  nav: {
+    about: string
+    skills: string
+    experience: string
+  }
+  ui: {
+    title_about: string
+    download_cv: string
+    send_email: string
+    call_me: string
+    next: string
+    prev: string
+  }
+  seo: {
+    default_description: string
+    experience_title: string
+    skills_title: string
+  }
+}
